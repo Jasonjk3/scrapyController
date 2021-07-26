05d74c6094a4c26a3852352117bec010016ebfa7
Jsonjk3 <451847790@qq.com>
2021-07-26 22:14:26 +0800

    project

--- application/api_v1/model/project.py
+++ application/api_v1/model/project.py
@@ -17,7 +17,9 @@ class Project(db.Model):
     __tablename__ = 'Project'
     # id = db.Column(db.String(255), primary_key=True)
     project_id = db.Column(db.Integer, primary_key=True,autoincrement=True)  # 项目id
-    # node_id = db.Column(db.String(255))
+    scrapy_id = db.Column(db.Integer)
+    scrapy_version  = db.Column(db.String(255))
+    # node_id = db.Column(db.Integer)
     # spider_id = db.Column(db.String(255))
     # task_id = db.Column(db.String(255))
     project_name = db.Column(db.String(255), unique=True)
@@ -34,8 +36,9 @@ class Spider(db.Model):
     # 定义表名
     __tablename__ = 'Spider'
     spider_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    scrapy_id = db.Column(db.String(255))
     # task_id = db.Column(db.String(255))
-    # node_id = db.Column(db.String(255))
+    node_id = db.Column(db.Integer)
     file_path = db.Column(db.String(255))
     spider_name = db.Column(db.String(255), unique=True)
     spider_config = db.Column(db.String(255))
@@ -53,9 +56,9 @@ class Task(db.Model):
     # 定义表名
     __tablename__ = 'Task'
     task_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    # spider_id = db.Column(db.String(255))
-    # node_id = db.Column(db.String(255))
-    # project_id = db.Column(db.String(255))
+    spider_id = db.Column(db.String(255))
+    node_id = db.Column(db.String(255))
+    project_id = db.Column(db.String(255))
     task_name = db.Column(db.String(255), unique=True)
     start_time = db.Column(db.String(255))  # 启动时间
     end_time = db.Column(db.String(255))
@@ -68,25 +71,36 @@ class Task(db.Model):
     status = db.Column(db.String(255))
 
 
-class TaskNodeList(db.Model):
-    """
-    任务下的节点表
-    """
-    # 定义表名
-    __tablename__ = 'TaskNodeList'
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    task_id = db.Column(db.Integer)
-    node_id = db.Column(db.Integer)
+# class TaskNodeList(db.Model):
+#     """
+#     任务下的节点表
+#     """
+#     # 定义表名
+#     __tablename__ = 'TaskNodeList'
+#     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     task_id = db.Column(db.Integer)
+#     node_id = db.Column(db.Integer)
 
-class ProjectSpiderList(db.Model):
+# class ProjectSpiderList(db.Model):
+#     """
+#     项目下的爬虫表
+#     """
+#     # 定义表名
+#     __tablename__ = 'ProjectSpiderList'
+#     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     project_id = db.Column(db.Integer)
+#     spider_id = db.Column(db.Integer)
+
+class ScrapySpiderList(db.Model):
     """
-    项目下的爬虫表
+    项目下scrapy的爬虫表
     """
     # 定义表名
-    __tablename__ = 'ProjectSpiderList'
+    __tablename__ = 'ScrapySpiderList'
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    project_id = db.Column(db.Integer)
+    scrapy_id = db.Column(db.Integer)
     spider_id = db.Column(db.Integer)
+
 
 if __name__ == '__main__':
     db.drop_all()
